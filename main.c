6ed5063ce9dc9015daa7eed205d1e23fb09af555
Pavel Kholkin <xpaul6@gmail.com>
2025-07-04 19:10:18 +0500

    feat: main menu

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// Structs
+// Typedefs
 typedef struct Object {
     Rectangle rec;
     Color color;
 } Object;
+
+typedef enum Mode {
+    normal,
+    ai,
+    unselected
+} Mode;
 
 // Constants
 #define TARGET_FPS 60
@@ -17,6 +23,10 @@ typedef struct Object {
 #define BALL_SIZE 15
 #define INIT_BALL_SPEED 300.0f
 #define BALL_ACCELERATION 10.0f
+
+#define NUM_MODES 2
+const Mode GAME_MODES[] = {normal, ai};
+const char* MODE_NAMES[] = {"Two players", "Versus AI"};
 
 // Global variables
 int g_windowWidth = 800;
@@ -30,7 +40,7 @@ int g_initBallY;
 
 // Functions
 
-void calculate_window_related_variables() {
+void calculate_window_related_variables(Rectangle menuRecs[NUM_MODES]) {
     g_windowWidth = GetScreenWidth();
     g_windowHeight = GetScreenHeight();
     g_initRacketLeftX = 0;
@@ -39,6 +49,9 @@ void calculate_window_related_variables() {
     g_initRacketRightY = g_windowHeight / 2 - RACKET_HEIGHT / 2;
     g_initBallX = g_windowWidth / 2 - BALL_SIZE / 2;
     g_initBallY = g_windowHeight / 2 - BALL_SIZE / 2;
+    for (int i = 0; i < NUM_MODES; i++) {
+        menuRecs[i] = (Rectangle){(float)g_windowWidth / 2 - 125, (float)g_windowHeight / 2 - 50 + i*80, 250, 60};
+    }
 }
 
 void handle_window_resize(Object* ball, Object* racketRight, Object* racketLeft) {
@@ -72,7 +85,7 @@ Vector2 init_ball_velocity() {
 }
 
 void render_pause_menu(bool* isPaused, Object ball, Object racketLeft, Object racketRight, int scoreLeft, int scoreRight) {
-if (IsKeyDown(KEY_SPACE)) {
+    if (IsKeyDown(KEY_SPACE)) {
         *isPaused = false;
         return;
     }
@@ -170,6 +183,11 @@ void reset_game(Object* ball, Object* racketLeft, Object* racketRight, Vector2* 
     *isPaused = true;
 }
 
+void reset_score(int* scoreLeft, int* scoreRight) {
+    *scoreLeft = 0;
+    *scoreRight = 0;
+}
+
 void render_game(Object ball, Object racketLeft, Object racketRight, int scoreLeft, int scoreRight) {
     BeginDrawing();
 
@@ -183,6 +201,36 @@ void render_game(Object ball, Object racketLeft, Object racketRight, int scoreLe
     EndDrawing();
 }
 
+void render_main_menu(Rectangle menuRecs[NUM_MODES], Mode* gameMode) {
+    int centerX = g_windowWidth / 2;
+    int centerY = g_windowHeight / 2;
+    int headerSize = 60;
+    int textSize = 30;
+    int hoveredRecIndex = -1;
+
+    for (int i = 0; i < NUM_MODES; i++) {
+        if (CheckCollisionPointRec(GetMousePosition(), menuRecs[i])) {
+            hoveredRecIndex = i;
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                *gameMode = GAME_MODES[i];
+            }
+        }
+    }
+
+    BeginDrawing();
+
+    ClearBackground(BLACK);
+
+    DrawText("raypong", centerX - MeasureText("raypong", headerSize) / 2, centerY - 200, headerSize, RAYWHITE);
+
+    for (int i = 0; i < NUM_MODES; i++) {
+        DrawRectangleLinesEx(menuRecs[i], 2.0f, i == hoveredRecIndex ? BLUE : RAYWHITE);
+        DrawText(MODE_NAMES[i], centerX - MeasureText(MODE_NAMES[i], textSize) / 2, centerY - 50 + i*80 + 15, textSize, i == hoveredRecIndex ? BLUE : RAYWHITE);
+    }
+
+    EndDrawing();
+}
+
 // Entry point
 int main() {
     // Window init
@@ -190,7 +238,17 @@ int main() {
     InitWindow(g_windowWidth, g_windowHeight, WINDOW_NAME); 
     SetTargetFPS(TARGET_FPS);
 
-    calculate_window_related_variables();
+    // Variables definition
+    int scoreLeft = 0;
+    int scoreRight = 0;
+    bool isGoal = false;
+    Vector2 ballVelocity = init_ball_velocity();
+    bool isPaused = true;
+    bool isResized = false;
+    Mode gameMode = unselected;
+    Rectangle menuRecs[NUM_MODES] = {0};
+
+    calculate_window_related_variables(menuRecs);
 
     // Objects init
     Object racketLeft = {
@@ -208,29 +266,57 @@ int main() {
         RAYWHITE
     };
 
-    // Variables definition
-    int scoreLeft = 0;
-    int scoreRight = 0;
-    bool isGoal = false;
-    Vector2 ballVelocity = init_ball_velocity();
-    bool isPaused = true;
-    bool isResized = false;
-
     // Main game loop
     while(!WindowShouldClose()) {
         if (IsWindowResized()) {
             isResized = true;
         }
 
-        if (isPaused) {
+        // Main menu handling
+        if (gameMode == unselected) {
+            SetExitKey(KEY_ESCAPE);
+            calculate_window_related_variables(menuRecs);
+            render_main_menu(menuRecs, &gameMode);
+            continue;
+        }
+        SetExitKey(KEY_NULL);
+
+        // NOTE: no ai implementaion yet
+        if (gameMode == ai) {
             if (isResized) {
-                calculate_window_related_variables();
+                calculate_window_related_variables(menuRecs);
+            }
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                gameMode = unselected;
+            }
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Work in progress", g_windowWidth / 2 - MeasureText("Work in progress", 60) / 2, g_windowHeight / 2 - 50, 60, RAYWHITE);
+            EndDrawing();
+            continue;
+        }
+
+        // Game pause handling
+        if (isPaused) {
+            // Resize handling
+            if (isResized) {
+                calculate_window_related_variables(menuRecs);
                 handle_window_resize(&ball, &racketRight, &racketLeft);
                 isResized = false;
             }
+
+            // Game escape handling
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                gameMode = unselected;
+                reset_game(&ball, &racketLeft, &racketRight, &ballVelocity, &isGoal, &isPaused);
+                reset_score(&scoreLeft, &scoreRight);
+            }
+
             render_pause_menu(&isPaused, ball, racketLeft, racketRight, scoreLeft, scoreRight);
             continue;
         }
+
+        // Game process
 
         double delta = GetFrameTime();
 
